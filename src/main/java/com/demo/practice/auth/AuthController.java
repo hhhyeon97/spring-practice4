7b2d3e34bf67fbe8896a37dd313e9b01157f773d
minsol <kth083030@naver.com>
2024-08-09 22:55:43 +0900

    JWT 다루기

--- src/main/java/com/demo/practice/auth/AuthController.java
+++ src/main/java/com/demo/practice/auth/AuthController.java
@@ -1,9 +1,13 @@
 package com.demo.practice.auth;
 
+import com.demo.practice.entity.UserRoleEnum;
+import com.demo.practice.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +17,15 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class AuthController {
 
     // HttpServletRequest에 넣을 쿠키에 담길 name 값을 상수로 설정
     public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // jwt 관련 유틸 di
+    private final JwtUtil jwtUtil;
 
     // 쿠키 만들기
     @GetMapping("/create-cookie")
@@ -60,7 +68,40 @@ public class AuthController {
         return "getSession : " + value;
     }
 
+    // jwt 생성
+    @GetMapping("/create-jwt")
+    public String createJwt(HttpServletResponse res) {
+        // Jwt 생성
+        String token = jwtUtil.createToken("Kim", UserRoleEnum.USER);
 
+        // Jwt 쿠키 저장
+        jwtUtil.addJwtToCookie(token, res);
+
+        return "createJwt : " + token;
+    }
+
+    // jwt 가져오기
+    @GetMapping("/get-jwt")
+    public String getJwt(@CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
+        // JWT 토큰 substring
+        String token = jwtUtil.substringToken(tokenValue);
+
+        // 토큰 검증
+        if(!jwtUtil.validateToken(token)){
+            throw new IllegalArgumentException("Token Error");
+        }
+
+        // 토큰에서 사용자 정보 가져오기
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        // 사용자 username
+        String username = info.getSubject();
+        System.out.println("username = " + username);
+        // 사용자 권한
+        String authority = (String) info.get(JwtUtil.AUTHORIZATION_KEY);
+        System.out.println("authority = " + authority);
+
+        return "getJwt : " + username + ", " + authority;
+    }
 
     // 쿠키 저장하는 메서드                             // 서블릿에서 만들어 준 response 객체 -> 여기에 데이터 담으면 클라이언트로 반환됨 !
     public static void addCookie(String cookieValue, HttpServletResponse res) {
