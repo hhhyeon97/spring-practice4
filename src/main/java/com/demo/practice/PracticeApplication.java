50f2e9e87a8b2c8ff228dfb73fa80ec5a9397d5c
minsol <kth083030@naver.com>
2024-08-11 20:54:28 +0900

    스프링 시큐리티 로그인 테스트

--- src/main/java/com/demo/practice/PracticeApplication.java
+++ src/main/java/com/demo/practice/PracticeApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
+@SpringBootApplication // 시큐리티 연습할 땐 다시 제외하는 부분 제거
 public class PracticeApplication {
 
     public static void main(String[] args) {
