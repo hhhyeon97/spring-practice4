350c181f681d9f55b30b69f8e877d50a7af11b50
minsol <kth083030@naver.com>
2024-08-12 01:10:14 +0900

    :white_check_mark: 스프링 시큐리티 : jwt 로그인

--- src/main/java/com/demo/practice/controller/UserController.java
+++ src/main/java/com/demo/practice/controller/UserController.java
@@ -41,15 +41,4 @@ public class UserController {
 
         return "redirect:/api/user/login-page";
     }
-
-    // 로그인
-    @PostMapping("/user/login")
-    public String login(LoginRequestDto requestDto, HttpServletResponse res){
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
-        return "redirect:/";
-    }
 }
